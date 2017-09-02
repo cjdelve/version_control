7ed179775a3292151041e463c9302b263aa16727
Cameron Delve <cjdelve.cd@gmail.com>
2017-09-02 14:46:27 -0500

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,9 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("this is a test of Git version control");
+    }
+}
